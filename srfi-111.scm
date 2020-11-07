2f1e5f8957d31ac36ec509c92c0ddfd24005d3be
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-11-07 07:32:12 -0800

    Checked in srfi-111.scm

---
+++ srfi-111.scm
@@ -0,0 +1,11 @@
+(import (only r7rs define-library))
+
+(define-library (srfi 111)
+  (import (scheme base))
+  (export box box? unbox set-box!)
+
+  (begin
+    (define-record-type box-type
+      (box value)
+      box?
+      (value unbox set-box!))))
